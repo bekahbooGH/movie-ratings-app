1dfcd0f737c284d2494baea6716314d0ce55d691
vagrant <vagrant@vagrant.vm>
2020-10-31 00:16:35 +0000

    Added the user seed db fxn in seed_db.py and ran it. Need to do queries to check it.

--- seed_database.py
+++ seed_database.py
@@ -3,7 +3,7 @@
 import os
 import json
 from random import choice, randint
-from datatime import datetime
+from datetime import datetime
 
 import crud
 import model
@@ -20,3 +20,35 @@ model.db.create_all()
 
 with open('data/movies.json') as f:
     movie_data = json.loads(f.read())
+
+# Create movies, store them in list so we can use them
+# to create fake ratings later
+movies_in_db = []
+for movie in movie_data:
+    # TODO: get the title, overview, and poster_path from the movie
+    # dictionary. Then, get the release_date and convert it to a
+    # datetime object with datetime.strptime
+    title, overview, poster_path = (movie['title'],
+                                    movie['overview'],
+                                    movie['poster_path'])
+    release_date = datetime.strptime(movie['release_date'], '%Y-%m-%d')
+
+    db_movie = crud.create_movie(title,
+                             overview,
+                             release_date,
+                             poster_path)
+    movies_in_db.append(db_movie)
+    # TODO: create a movie here and append it to movies_in_db
+
+# Create 10 users; each user will make 10 ratings
+for n in range(10):
+    email = f'user{n}@test.com'  # Voila! A unique email!
+    password = 'test'
+
+    user = crud.create_user(email, password)
+
+    for _ in range(10):
+        random_movie = choice(movies_in_db)
+        score = randint(1, 5)
+
+        crud.create_rating(user, random_movie, score)
